ed130b709bca24244e7ce17b04ca23d9306e9b6c
PierceLe <haichau.hvsg@gmail.com>
2024-10-31 12:13:36 +1100

    chore: updated the state pattern

--- src/main/java/pacman/model/entity/dynamic/ghost/state/FrightenedModeState.java
+++ src/main/java/pacman/model/entity/dynamic/ghost/state/FrightenedModeState.java
@@ -13,7 +13,7 @@ import static java.lang.Math.pow;
 public class FrightenedModeState implements GhostModeState {
     private final Ghost ghost;
     private static double MODE_LENGTH = 0;
-    private double duration;
+    private double timeFrame;
     private static final int POINT = 100;
 
     private final Image image =  new Image("maze/ghosts/frightened.png");
@@ -31,11 +31,11 @@ public class FrightenedModeState implements GhostModeState {
     public void handleCollision(Level level, Renderable renderable) {
         if (level.isPlayer(renderable)) {
             ghost.reset();
-            this.duration = MODE_LENGTH;
+            this.timeFrame = MODE_LENGTH;
             ghost.setGhostMode(GhostMode.SCATTER);
             ghost.setState(GhostMode.SCATTER);
             ghost.playingRespawn();
-            this.duration = MODE_LENGTH;
+            this.timeFrame = MODE_LENGTH;
             level.incrementGhostStreak();
             int streak = level.getStreakCount();
             int base = (int) pow(2, streak);
@@ -44,8 +44,8 @@ public class FrightenedModeState implements GhostModeState {
     }
     @Override
     public void update() {
-        if (this.duration <= 0){
-            this.duration = MODE_LENGTH;
+        if (this.timeFrame <= 0){
+            this.timeFrame = MODE_LENGTH;
             ghost.setGhostMode(GhostMode.SCATTER);
             ghost.setState(GhostMode.SCATTER);
             return;
@@ -53,17 +53,17 @@ public class FrightenedModeState implements GhostModeState {
         updateDirection(ghost);
         ghost.getKinematicState().update();
         ghost.getBoundingBox().setTopLeft(ghost.getKinematicState().getPosition());
-        this.duration -= 1;
+        this.timeFrame -= 1;
     }
 
     @Override
     public void collectPowerPellets() {
-        this.duration = MODE_LENGTH;
+        this.timeFrame = MODE_LENGTH;
     }
 
 
     public void setModeLength(double modeLength) {
-        this.duration = modeLength;
+        this.timeFrame = modeLength;
         System.out.println(modeLength);
         MODE_LENGTH = modeLength;
     }
